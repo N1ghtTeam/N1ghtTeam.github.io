39e16448a4c8900be9ba005021569700cbc244ed
dnblue-code <68686710+dnblue-code@users.noreply.github.com>
2021-11-02 14:37:07 +0700

    use WebComponent for web-header.

---
+++ scripts/header.js
@@ -0,0 +1,33 @@
+class Header extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback() {
+        
+        this.innerHTML=`
+        
+            <header>
+                <nav>
+                    <nav>
+
+                        <h2 onclick="window.location.href='./index.html';">NFramework</h2>
+                        <p id="header-framework-version">&nbsp;&nbsp;v1.0</p>
+
+                    </nav>
+                    <ul>
+                        <input type="text" placeholder="Search...">
+                        <li><a  onclick="window.location.href='./docs.html';">Docs</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#"><i class="fas fa-language"></i> Languages</a></li>
+                    </ul>
+                </nav>
+
+            </header>
+        
+        `;
+
+    }
+
+}
+customElements.define('web-header', Header);
